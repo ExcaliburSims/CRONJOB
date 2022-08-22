dee8aad6b83185d0690d54067f7905ca9c7e3f15
ExcaliburSims <ximeonzilu@gmail.com>
2022-08-22 19:46:06 +0100

    feat: use shell to execute directory of project

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let CronJob = require('node-cron');
 let shell = require('shelljs');
 
 
-CronJob.schedule('2-6 * * * * *', () => {
-		console.log('You will see this message every second');
+CronJob.schedule('* * * * * *', () => {
+	if (shell.exec('dir').code !== 0) {
+			console.log('quelque chose ne va pas');
+		}
 	}
 );
